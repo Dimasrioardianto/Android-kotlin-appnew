1a1ee5daaaa6c8c8e8828c8bd3030e02f4278840
DimasRioArdianto <daffaprasetyo2017@gmail.com>
2024-10-08 09:57:08 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/polbeng/Ardianto/myaplication1/MainActivity.kt
@@ -0,0 +1,38 @@
+package id.ac.polbeng.Ardianto.myaplication1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+class MainActivity : AppCompatActivity() {
+    private lateinit var tvCounter: TextView
+    private lateinit var btnAddNumber: Button
+    private lateinit var btnSubtractNumber: Button
+    private lateinit var btnResetNumber: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tvCounter = findViewById(R.id.tvCounter)
+        btnAddNumber = findViewById(R.id.btnAddNumber)
+        btnSubtractNumber = findViewById(R.id.btnSubtractNumber)
+        btnResetNumber = findViewById(R.id.btnResetNumber)
+        tvCounter.text = "1"
+    }
+
+    fun addNumber(v: View) {
+        val currVal = tvCounter.text.toString().toInt()
+        val nextVal = currVal + 1
+        tvCounter.text = nextVal.toString()
+    }
+
+    fun subtract_Number(v: View) {
+        val currVal = tvCounter.text.toString().toInt()
+        val nextVal = currVal - 1
+        tvCounter.text = nextVal.toString()
+    }
+
+    fun reset_Number(v: View) {
+        tvCounter.text = "0"
+    }
+}
